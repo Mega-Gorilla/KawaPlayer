3e55a94df4f03d6b7e20921db148d7bd835a3b8e
kw <koorimizuw@gmail.com>
2026-04-08 19:03:12 +0900

    fix: remove game object with undo after add yamaplayer with menu

--- Editor/Menu/YamaPlayerMenu.cs
+++ Editor/Menu/YamaPlayerMenu.cs
@@ -6,31 +6,31 @@ namespace Yamadev.YamaStream.Editor
   public static class YamaPlayerMenu
   {
     const string menuPrefix = "GameObject/YamaPlayer/";
-    static string _yamaplayerPrefabGuid = "0ca92d0fbf2bf3944bfeef01f4977da5";
-    static string _yamaplayerNoScreenPrefabGuid = "84f33e64f5807174288ea98464197fd1";
-    static string _subScreenPrefabGuid = "1d1c026d8b023d04ea81f85594f05aec";
-    static string _controllerBarPrefabGuid = "ddf7f58d0d20d6843a79711f81f34bf2";
-    static string _playlistPanelPrefabGuid = "32aa1985af9229540a44cf22406ee1a2";
+    private static readonly string yamaplayerPrefabGuid = "0ca92d0fbf2bf3944bfeef01f4977da5";
+    private static readonly string yamaplayerNoScreenPrefabGuid = "84f33e64f5807174288ea98464197fd1";
+    private static readonly string subScreenPrefabGuid = "1d1c026d8b023d04ea81f85594f05aec";
+    private static readonly string controllerBarPrefabGuid = "ddf7f58d0d20d6843a79711f81f34bf2";
+    private static readonly string playlistPanelPrefabGuid = "32aa1985af9229540a44cf22406ee1a2";
 
     [MenuItem(menuPrefix + "Main (With screen and UI)", priority = 1)]
     public static void CreateYamaPlayer() =>
-        CreateGameObject(AssetDatabase.GUIDToAssetPath(_yamaplayerPrefabGuid));
+        CreateGameObject(AssetDatabase.GUIDToAssetPath(yamaplayerPrefabGuid));
 
     [MenuItem(menuPrefix + "Main (No screen)", priority = 2)]
     public static void CreateYamaPlayerNoScreen() =>
-        CreateGameObject(AssetDatabase.GUIDToAssetPath(_yamaplayerNoScreenPrefabGuid));
+        CreateGameObject(AssetDatabase.GUIDToAssetPath(yamaplayerNoScreenPrefabGuid));
 
     [MenuItem(menuPrefix + "SubScreen", priority = 101)]
     public static void CreateSubScreen() =>
-        CreateGameObject(AssetDatabase.GUIDToAssetPath(_subScreenPrefabGuid));
+        CreateGameObject(AssetDatabase.GUIDToAssetPath(subScreenPrefabGuid));
 
     [MenuItem(menuPrefix + "Controller Bar", priority = 102)]
     public static void CreateControllerBar() =>
-        CreateGameObject(AssetDatabase.GUIDToAssetPath(_controllerBarPrefabGuid));
+        CreateGameObject(AssetDatabase.GUIDToAssetPath(controllerBarPrefabGuid));
 
     [MenuItem(menuPrefix + "Playlist Panel", priority = 103)]
     public static void CreatePlaylistPanel() =>
-        CreateGameObject(AssetDatabase.GUIDToAssetPath(_playlistPanelPrefabGuid));
+        CreateGameObject(AssetDatabase.GUIDToAssetPath(playlistPanelPrefabGuid));
 
     static void CreateGameObject(string path)
     {
@@ -42,6 +42,7 @@ namespace Yamadev.YamaStream.Editor
       if (obj == null) return;
 
       obj.name = GameObjectUtility.GetUniqueNameForSibling(parent, prefab.name);
+      Undo.RegisterCreatedObjectUndo(obj, $"Create {obj.name}");
       Selection.activeGameObject = obj;
     }
   }
